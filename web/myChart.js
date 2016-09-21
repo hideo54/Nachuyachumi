4db35a48b8e0f7e638fbf6cc94be0e8cd71a67a3
hideo54 <hideo54@users.noreply.github.com>
2016-09-22 02:06:32 +0900

    微分法完了
    つらい

--- web/myChart.js
+++ web/myChart.js
@@ -42,7 +42,7 @@ var homeworkChart = new Chart(document.getElementById('homework'), {
         datasets: [{
             label: '進捗(%)',
             data: [
-                100 * 22 / 42,
+                100 * 28 / 42,
                 100 * 0 / 58,
                 100 * 12 / 12,
                 100 * 3 / 20,
